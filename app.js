3e0d68e75cafaaace5f922e384c35878b4c8fc13
SOOYONG <76885280+SooYong-Jeong@users.noreply.github.com>
2021-07-17 20:51:37 +0900

    object

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
-console.log(daysOfWeek);
-
-daysOfWeek.push("sun");
-console.log(daysOfWeek);
+const player = {
+    name : "intragon",
+    points : 24,
+    fat : false,
+};
+console.log(player);
+player.fat = true;
+player.sex = "male";
+console.log(player);
