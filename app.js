b468a3dce19f3ac23948ee92815fddf13798b839
SOOYONG <76885280+SooYong-Jeong@users.noreply.github.com>
2021-07-19 22:19:03 +0900

    RETURN

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 const clacualter = {
     plus: function(a, b)
     {
-        console.log(a+b);
+        return a+b;
     },
     minus: function(a, b)
     {
-        console.log(a-b);
+        return a-b;
     },
     devide: function(a, b)
     {
-        console.log(a/b);
+        return a/b;
     },
     multiple: function(a, b)
     {
-        console.log(a*b);
+        return a*b;
     },
     powerof: function(a, b)
     {
-        console.log(a**b);
+        return a**b;
     },
 };
-clacualter.plus(1,2);
-clacualter.minus(1,2);
-clacualter.devide(1,2);
-clacualter.multiple(1,2);
-clacualter.powerof(1,2);
+console.log(clacualter.plus(1,2));
+console.log(clacualter.minus(1,2));
+console.log(clacualter.devide(1,2));
+console.log(clacualter.multiple(1,2));
+console.log(clacualter.powerof(1,2));
