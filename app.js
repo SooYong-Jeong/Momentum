3cfc7bde8f09501071520752a6204c04df018bdf
SOOYONG <76885280+SooYong-Jeong@users.noreply.github.com>
2021-07-25 21:07:30 +0900

    drink

--- app.js
+++ app.js
@@ -1,27 +1,12 @@
-const clacualter = {
-    plus: function(a, b)
-    {
-        return a+b;
-    },
-    minus: function(a, b)
-    {
-        return a-b;
-    },
-    devide: function(a, b)
-    {
-        return a/b;
-    },
-    multiple: function(a, b)
-    {
-        return a*b;
-    },
-    powerof: function(a, b)
-    {
-        return a**b;
-    },
-};
-console.log(clacualter.plus(1,2));
-console.log(clacualter.minus(1,2));
-console.log(clacualter.devide(1,2));
-console.log(clacualter.multiple(1,2));
-console.log(clacualter.powerof(1,2));
+const age = parseInt(prompt("How old?"));
+console.log(typeof age);
+
+if (isNaN(age)) {
+    console.log("pls write number");
+}
+else if(age < 18){
+    console.log("can`t drink");
+}
+else {
+    console.log("ok");
+}
